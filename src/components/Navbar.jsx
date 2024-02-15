1c63d48915d5bad8481e4ea7f005dddde8ab63ed
KrishKoria <krishkoria2004@gmail.com>
2024-02-15 13:38:59 +0530

    Added Basic Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/Navbar.css"
+// import data from "../data/data.json"
+
+export const Navbar = () => {
+    return (
+        <div className="nav">
+            <div className="logo">
+             <a href="/">
+                  <img id="logoImage" src="" alt="Not Found" /> 
+             </a>
+            </div>
+            <a className="navlinks" href="/">Join Us</a>
+            <a className="navlinks" href="/">Login</a>
+        </div>
+    )
+}
